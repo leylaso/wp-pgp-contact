e214ae47e215d8af5ba9e57832cf3ae0b1e4e7f3
Sofian Benaissa <me@sfyn.net>
2014-11-03 08:33:12 -0500

    Improve key info verification for the get key form

--- js/verify-key.js
+++ js/verify-key.js
@@ -5,19 +5,57 @@
 (function ($) {
 
 	$(document).ready(function(){
+
+		if (getKey() != null) 
+			showKeyInfo(0);
+		else 
+			init();
+
 		$('input#verify').click( function(event) {
 			event.preventDefault();
-			rawKey = $('textarea#public-key').val();
-			actKey = window.openpgp.key.readArmored(rawKey).keys[0];
-			fp = actKey.primaryKey.fingerprint.match(new RegExp('.{1,4}', 'g')).join(" ");
-			$('input#key-print').val(fp.toUpperCase());
-			$('input#key-algo').val(actKey.primaryKey.algorithm.toUpperCase());
-			$('input#key-user').val(actKey.getPrimaryUser().user.userId.userid);
-			$('table#dynamic-key-info').fadeIn();
-			$('input#submit').fadeIn();
+			showKeyInfo();
 		});
-		$('input#verify').show();
+
+		$('textarea#public-key').change( function() {
+			init();
+		});
+
+	});
+
+	function init() {
+		$('input#key-print').val('');
+		$('input#key-algo').val('');
+		$('input#key-user').val('');
 		$('input#submit').hide();
 		$('table#dynamic-key-info').hide();
-	});
+		$('input#verify').show();
+	}
+
+	function getKey() {
+		keyData =  openpgp.key.readArmored($('textarea#public-key').val());;
+		if (typeof keyData.err == 'undefined')
+			return keyData.keys[0];
+		else
+			return null;
+	}
+
+	function showKeyInfo(fTime) {
+		keyZero = getKey();
+		if (keyZero==null) {
+			alert('This key is invalid!');	// TODO localize this string
+		}
+		else {
+			fTime = typeof fTime !== 'undefined' ? fTime : 400;
+			fp = keyZero.primaryKey.fingerprint.match(new RegExp('.{1,4}', 'g')).join(" ");
+			$('input#key-print').val(fp.toUpperCase());
+			$('input#key-algo').val(keyZero.primaryKey.algorithm.toUpperCase());
+			$('input#key-user').val(keyZero.getPrimaryUser().user.userId.userid);
+			if ($('table#dynamic-key-info').is(':hidden'))
+				$('table#dynamic-key-info').fadeIn(fTime);
+			if ($('input#submit').is(':hidden'))
+				$('input#submit').fadeIn(fTime);
+		}
+		
+	}
+
 })(jQuery);
