183eb9d9fc66ff5409c534a4a2d5fb17fa63f362
sfyn <sfyn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-06 20:54:23 +0000

    Merge branch 'master' of github.com:sfyn/wp-pgp-contact
    git-svn-id: http://plugins.svn.wordpress.org/pgp-contact/trunk@1021178 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/verify-key.js
@@ -0,0 +1,61 @@
+/**
+ * @file
+ * Sets up the dynamic verification on key upload pages.
+ */
+(function ($) {
+
+	$(document).ready(function(){
+
+		if (getKey() != null) 
+			showKeyInfo(0);
+		else 
+			init();
+
+		$('input#verify').click( function(event) {
+			event.preventDefault();
+			showKeyInfo();
+		});
+
+		$('textarea#public-key').change( function() {
+			init();
+		});
+
+	});
+
+	function init() {
+		$('input#key-print').val('');
+		$('input#key-algo').val('');
+		$('input#key-user').val('');
+		$('input#submit').hide();
+		$('table#dynamic-key-info').hide();
+		$('input#verify').show();
+	}
+
+	function getKey() {
+		keyData =  openpgp.key.readArmored($('textarea#public-key').val());;
+		if (typeof keyData.err == 'undefined')
+			return keyData.keys[0];
+		else
+			return null;
+	}
+
+	function showKeyInfo(fTime) {
+		keyZero = getKey();
+		if (keyZero==null) {
+			alert('This key is invalid!');	// TODO localize this string
+		}
+		else {
+			fTime = typeof fTime !== 'undefined' ? fTime : 400;
+			fp = keyZero.primaryKey.fingerprint.match(new RegExp('.{1,4}', 'g')).join(" ");
+			$('input#key-print').val(fp.toUpperCase());
+			$('input#key-algo').val(keyZero.primaryKey.algorithm.toUpperCase());
+			$('input#key-user').val(keyZero.getPrimaryUser().user.userId.userid);
+			if ($('table#dynamic-key-info').is(':hidden'))
+				$('table#dynamic-key-info').fadeIn(fTime);
+			if ($('input#submit').is(':hidden'))
+				$('input#submit').fadeIn(fTime);
+		}
+		
+	}
+
+})(jQuery);
---
+++ pgp-contact.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * PGP Contact Forms
+ *
+ * A WordPress plugin used to provide encrypted email contact forms on a per-
+ * user basis.
+ *
+ * @author		Sofian Benaissa <me@sfyn.net>
+ * @package		PGP Contact
+ * @license		GPL-3.0+
+ * @copyright 	2014 Sofian Benaissa
+ *
+ * @wordpress-plugin
+ * Plugin Name: 	PGP Contact Forms
+ * Plugin URI: 		https://github.com/sfyn/wp-pgp-contact
+ * Description: 	Allows users to upload PGP public keys and receive encrypted email via contact forms.
+ * Version: 		1.0
+ * Author: 			Sofian Benaissa
+ * Author URI: 		http://www.sfyn.net/
+ * License: 		GPL-3.0+
+ * License URI:		http://www.gnu.org/licenses/
+ * Text Domain: 	pgp-contact
+ * 
+ */
+/*
+ * This file is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This file is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details. You can consult the license at http://www.gnu.org/licenses/
+ */
+
+if (!defined('WPINC'))
+	die;
+
+require_once(plugin_dir_path(__FILE__) . 'pgp-contact.cls');
+add_action('plugins_loaded', array('PGP_Contact', 'get_instance'));
+?>
